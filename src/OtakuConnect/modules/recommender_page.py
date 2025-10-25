b4c82c94a3e85a123a38c8bdf11fc4c36dcb376c
Anurag Mallick <anuragmallick7447@gmail.com>
2025-10-25 16:45:21 -0400

    Initial commit recommender page

--- src/OtakuConnect/modules/recommender_page.py
+++ src/OtakuConnect/modules/recommender_page.py
@@ -3,3 +3,17 @@ import streamlit as st
 def recommender():
     st.title(":red[OUR RECOMMENDATION]")
     st.write("Recommendation system output goes here.")
+
+
+def shuffle_recommender(user_preferences, user_history, user_feedback):
+    """
+    Function to get the content for the recommender page.
+    Inputs:
+        List of user preferences
+        List of user history
+        List of user comments, ratings, likes, dislikes
+    Returns:
+        Recommended content for the user based on their preferences and history.
+    """
+
+    
